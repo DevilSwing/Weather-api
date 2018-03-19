af78c712e2700008573f8ee364d61f3ac8abf497
bananaWeb <a89.volkov@gmail.com>
2018-03-19 18:57:33 +0300

    weather

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+ let city = $('.weather-city'),
+ 	 degree = $('.weather-degree'),
+ 	 wind = $('.weather-wind'),
+ 	 forecast = $('.weather-forecast'),
+ 	 input = $('.js-cityId'),
+ 	 btn = $('.js-search');
+
+	$.getJSON('http://api.openweathermap.org/data/2.5/weather?id=498817&APPID=f8cbc3ace5cb982a519ff23088731dba' , function(result) {
+		setWeatherData(result);
+
+		
+	})
+	function setWeatherData(result){
+		city.text(result.name);
+		degree.text(result.main.temp);
+		wind.text(result.wind.speed);
+		forecast.text(result.weather[0].description)
+	}
+btn.click (function() {
+	$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + input.val() + '&APPID=f8cbc3ace5cb982a519ff23088731dba' , function(result) {
+		setWeatherData(result);
+	})
+})
+
+});
